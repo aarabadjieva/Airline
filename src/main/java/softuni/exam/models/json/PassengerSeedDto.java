33e48bf62d5e7c7e8e74f632c3371634268c6087
lin <l.i.n@abv.bg>
2020-04-06 16:57:06 +0300

    initial commit

---
+++ src/main/java/softuni/exam/models/json/PassengerSeedDto.java
@@ -0,0 +1,46 @@
+package softuni.exam.models.json;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+public class PassengerSeedDto {
+
+    @Expose
+    @NotNull
+    @Length(min = 2)
+    private String firstName;
+
+    @Expose
+    @NotNull
+    @Length(min = 2)
+    private String lastName;
+
+    @Expose
+    @NotNull
+    @Min(1)
+    private int age;
+
+    @Expose
+    @NotNull
+    private String phoneNumber;
+
+    @Expose
+    @NotNull
+    @Pattern(regexp = ".*@.*\\..*")
+    private String email;
+
+    @Expose
+    @SerializedName("town")
+    @NotNull
+    @Length(min = 2)
+    private String townName;
+}
